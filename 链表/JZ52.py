bda88f98ed3f3291635a352fb44262a8c796b2ae
goen-kkk <52314100+goen-kkk@users.noreply.github.com>
2023-05-26 10:18:35 +0800

    Create JZ52.py

---
+++ 链表/JZ52.py
@@ -0,0 +1,22 @@
+# 输入两个无环的单向链表，找出它们的第一个公共结点，如果没有公共节点则返回空。
+# 先遍历一条链表，记录所有节点，再遍历第二条，找到第一个公共节点
+
+# @param pHead1 ListNode类 
+# @param pHead2 ListNode类 
+# @return ListNode类
+#
+class Solution:
+    def FindFirstCommonNode(self , pHead1 , pHead2 ):
+        # write code here
+        p = pHead1
+        q = pHead2
+        set_A = set()
+        while(p!=None):
+            set_A.add(p)
+            p = p.next
+        while(q!=None):
+            if q in set_A:
+                return q 
+            q = q.next
+        return None
+        
